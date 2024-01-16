ed38d2eb4ac20f7ed35a5557163826a79059b49e
Egor Nikitin <60084256+BenderEg@users.noreply.github.com>
2024-01-17 00:15:22 +0400

    Billing models (#23)

--- src/billing/admin.py
+++ src/billing/admin.py
@@ -1,1 +1,30 @@
 from django.contrib import admin  # noqa
+
+from billing.models import Payments, Refunds
+
+
+@admin.register(Payments)
+class PaymentsAdmin(admin.ModelAdmin):
+    list_display = (
+        "user_id",
+        "external_payment_id",
+        "payment_method",
+        "amount",
+        "currency",
+        "status",
+        "refundable",
+        "recurrent",
+    )
+    list_display_links = ("user_id", "external_payment_id")
+    ordering = ["created_at"]
+    search_fields = ["user_id", "external_payment_id"]
+    list_per_page = 30
+
+
+@admin.register(Refunds)
+class RefundsAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "external_refund_id", "amount", "currency", "status", "reason")
+    list_display_links = ("user_id", "external_refund_id")
+    ordering = ["created_at"]
+    search_fields = ["user_id", "external_refund_id"]
+    list_per_page = 30
--- src/billing/models.py
+++ src/billing/models.py
@@ -4,7 +4,10 @@ from django.db import models
 from django.db.models.functions import Now
 from django.utils.translation import gettext_lazy as _
 
+# from models_mixins import TimeStampedMixin, UUIDMixin
 
+
+# удалить миксины после merge, раскоментировать импорт миксинов
 class TimeStampedMixin(models.Model):
     created_at = models.DateTimeField(_("created_at"), auto_now_add=True, db_default=Now())
     updated_at = models.DateTimeField(_("updated_at"), auto_now=True, db_default=Now())
@@ -18,3 +21,62 @@ class UUIDMixin(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Currency(models.Choices):
+    rub = "RUB"
+    usd = "USD"
+    eur = "EUR"
+
+
+class Status(models.Choices):
+    succeeded = "succeeded"
+    canceled = "canceled"
+    pending = "pending"
+
+
+class Payments(UUIDMixin, TimeStampedMixin):
+    user_id = models.UUIDField(_("user_id"), blank=False, null=False)
+    user_purchase_item_id = models.UUIDField(_("user_purchase_item_id"), blank=False, null=False)
+    external_payment_id = models.CharField(_("external_payment_id"), blank=False, null=False)
+    payment_method = models.CharField(_("payment_method"), blank=True)
+    payment_method_id = models.CharField(_("payment_method_id"), blank=True)
+    amount = models.FloatField(_("amount"), blank=False)
+    currency = models.CharField(_("currency"), choices=Currency.choices, blank=False, null=False)
+    status = models.CharField(_("status"), choices=Status.choices, blank=False, null=False, default=Status.pending)
+    recurrent = models.BooleanField(_("recurrent"))
+    refundable = models.BooleanField(_("refundable"))
+
+    class Meta:
+        db_table = 'public"."payments'
+        verbose_name = _("payments")
+        verbose_name_plural = _("payments")
+        indexes = [
+            models.Index(fields=["user_id", "user_purchase_item_id"], name="user_id_item_id_idx"),
+            models.Index(fields=["external_payment_id"], name="external_payment_idx"),
+        ]
+
+    def __str__(self) -> str:
+        return self.id
+
+
+class Refunds(UUIDMixin, TimeStampedMixin):
+    user_id = models.UUIDField(_("user_id"), blank=False, null=False)
+    payment = models.ForeignKey(Payments, on_delete=models.RESTRICT, blank=False, null=False)
+    external_refund_id = models.CharField(_("external_refund_id"))
+    amount = models.FloatField(_("amount"), blank=False)
+    currency = models.CharField(_("currency"), choices=Currency.choices, blank=False, null=False)
+    status = models.CharField(_("status"), choices=Status.choices, blank=False, null=False, default=Status.pending)
+    reason = models.TextField(_("reason"))
+
+    class Meta:
+        db_table = 'public"."refunds'
+        verbose_name = _("refunds")
+        verbose_name_plural = _("refunds")
+        indexes = [
+            models.Index(fields=["user_id", "payment"], name="user_id_payment_id_idx"),
+            models.Index(fields=["external_refund_id"], name="external_refund_idx"),
+        ]
+
+    def __str__(self) -> str:
+        return self.id
